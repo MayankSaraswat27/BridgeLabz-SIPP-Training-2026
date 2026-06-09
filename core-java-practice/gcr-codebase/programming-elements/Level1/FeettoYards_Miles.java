1927e1f83d87f550d11594eadc0d0fbccae584c8
unknown <mayanksaraswat215@gmail.com>
2026-06-09 09:54:38 +0530

    [Mayank] Added: Feet To Yards & Miles

---
+++ core-java-practice/gcr-codebase/programming-elements/Level1/FeettoYards_Miles.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class FeettoYards_Miles {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double feet = input.nextDouble();
+
+        double yards = feet / 3;
+        double miles = yards / 1760;
+
+        System.out.println("Distance in yards is " + yards +
+                " and in miles is " + miles);
+    }
+}
