41ef0a887d11bdfe433127597c40fea29c47caf5
unknown <mayanksaraswat215@gmail.com>
2026-06-17 13:18:49 +0530

    [Mayank] Added: Factors of a Number Using While Loop

---
+++ core-java-practice/gcr-codebase/control-flow/Level2/FactorsofaNumber_While_Loop.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class FactorsofaNumber_While_Loop {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int number = sc.nextInt();
+
+        int i = 1;
+
+        while (i < number) {
+            if (number % i == 0) {
+                System.out.print(i + " ");
+            }
+            i++;
+        }
+    }
+}
