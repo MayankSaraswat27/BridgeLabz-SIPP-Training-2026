48c1edc99ba0cb4cb6863a8c6eb2a5948a120485
unknown <mayanksaraswat215@gmail.com>
2026-06-23 13:45:24 +0530

    [Mayank] Added : Student Vote Checker

---
+++ core-java-practice/gcr-codebase/methods/Level2/StudentVoteChecker.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class StudentVoteChecker {
+
+    public boolean canStudentVote(int age) {
+
+        if(age < 0)
+            return false;
+
+        return age >= 18;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] ages = new int[10];
+
+        StudentVoteChecker obj = new StudentVoteChecker();
+
+        for(int i = 0; i < ages.length; i++) {
+
+            ages[i] = sc.nextInt();
+
+            if(obj.canStudentVote(ages[i]))
+                System.out.println("Can Vote");
+            else
+                System.out.println("Cannot Vote");
+        }
+    }
+}
