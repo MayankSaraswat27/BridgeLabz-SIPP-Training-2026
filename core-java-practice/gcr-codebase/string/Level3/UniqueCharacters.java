a6847d1f278c07135c8c075bfaed73131d093d07
unknown <mayanksaraswat215@gmail.com>
2026-06-23 13:06:35 +0530

    [Mayank] Added : Unique Characters

---
+++ core-java-practice/gcr-codebase/string/Level3/UniqueCharacters.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class UniqueCharacters {
+
+    static int length(String text) {
+        int count = 0;
+
+        try {
+            while (true) {
+                text.charAt(count++);
+            }
+        } catch (Exception e) {
+        }
+
+        return count - 1;
+    }
+
+    static char[] uniqueCharacters(String text) {
+
+        int len = length(text);
+
+        char[] temp = new char[len];
+        int index = 0;
+
+        for (int i = 0; i < len; i++) {
+
+            boolean unique = true;
+
+            for (int j = 0; j < i; j++) {
+                if (text.charAt(i) == text.charAt(j)) {
+                    unique = false;
+                    break;
+                }
+            }
+
+            if (unique) {
+                temp[index++] = text.charAt(i);
+            }
+        }
+
+        char[] result = new char[index];
+
+        for (int i = 0; i < index; i++) {
+            result[i] = temp[i];
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String text = sc.nextLine();
+
+        char[] result = uniqueCharacters(text);
+
+        for (char ch : result) {
+            System.out.print(ch + " ");
+        }
+    }
+}
