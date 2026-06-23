b86f4dcb9725ac2d9065f5549d6c8d2254e3cb0d
unknown <mayanksaraswat215@gmail.com>
2026-06-23 12:42:54 +0530

    [Mayank] Added : Lower Case Demo

---
+++ core-java-practice/gcr-codebase/string/Level1/LowerCaseDemo.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class LowerCaseDemo {
+
+    static String convertLower(String text) {
+        String result = "";
+
+        for (int i = 0; i < text.length(); i++) {
+            char ch = text.charAt(i);
+
+            if (ch >= 'A' && ch <= 'Z') {
+                ch = (char) (ch + 32);
+            }
+
+            result += ch;
+        }
+
+        return result;
+    }
+
+    static boolean compareStrings(String s1, String s2) {
+        if (s1.length() != s2.length())
+            return false;
+
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i))
+                return false;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String text = sc.nextLine();
+
+        String lower1 = convertLower(text);
+        String lower2 = text.toLowerCase();
+
+        System.out.println(compareStrings(lower1, lower2));
+    }
+}
