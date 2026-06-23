0edbebfb6c02df0c6768ab5c2600fcc5bdcbce9d
unknown <mayanksaraswat215@gmail.com>
2026-06-23 13:29:52 +0530

    [Mayank] Added : Sum Natural Numbers

---
+++ core-java-practice/gcr-codebase/methods/Level1/SumNaturalNumbers.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class SumNaturalNumbers {
+
+    static int sum(int n) {
+
+        int total = 0;
+
+        for (int i = 1; i <= n; i++) {
+            total += i;
+        }
+
+        return total;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        System.out.println(sum(n));
+    }
+}
