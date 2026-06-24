daddcb752c6b79908d1bfaf63d8a61966f6671e5
unknown <mayanksaraswat215@gmail.com>
2026-06-24 09:47:14 +0530

    [Mayank] Added : Nested Try Catch

---
+++ core-java-practice/gcr-codebase/exception-handling/NestedTryCatch.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class NestedTryCatch {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = {10, 20, 30, 40};
+
+        int index = sc.nextInt();
+        int divisor = sc.nextInt();
+
+        try {
+
+            try {
+
+                int value = arr[index];
+
+                try {
+
+                    System.out.println(
+                            value / divisor);
+
+                } catch (ArithmeticException e) {
+
+                    System.out.println(
+                            "Cannot divide by zero!");
+                }
+
+            } catch (
+                    ArrayIndexOutOfBoundsException e) {
+
+                System.out.println(
+                        "Invalid array index!");
+            }
+
+        } catch (Exception e) {
+
+            System.out.println(
+                    "Unexpected error");
+        }
+    }
+}
