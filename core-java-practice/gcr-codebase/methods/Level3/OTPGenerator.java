98a6fbafe91f64c6ef7ea6e76690ea0f9982128b
unknown <mayanksaraswat215@gmail.com>
2026-06-24 09:10:45 +0530

    [Mayank] Added : OTP Generator

---
+++ core-java-practice/gcr-codebase/methods/Level3/OTPGenerator.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class OTPGenerator {
+
+    static int generateOTP() {
+        return 100000 + (int)(Math.random() * 900000);
+    }
+
+    static boolean areUnique(int[] otps) {
+
+        for(int i = 0; i < otps.length; i++) {
+            for(int j = i + 1; j < otps.length; j++) {
+                if(otps[i] == otps[j])
+                    return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        int[] otps = new int[10];
+
+        for(int i = 0; i < 10; i++) {
+            otps[i] = generateOTP();
+        }
+
+        System.out.println(Arrays.toString(otps));
+        System.out.println("All Unique = " + areUnique(otps));
+    }
+}
